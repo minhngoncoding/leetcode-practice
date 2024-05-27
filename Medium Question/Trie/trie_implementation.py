e9c38fc903acfbc9ea478acd9db0394397554ba6
minhngonmcoding <minhngon.dang1@gmail.com>
2024-05-27 17:48:23 +0700

    Create trie_implementation.py
    Trie data structure

---
+++ Medium Question/Trie/trie_implementation.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.end_of_word = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        if node.end_of_word:
+            return True
+        else:
+            return False
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
